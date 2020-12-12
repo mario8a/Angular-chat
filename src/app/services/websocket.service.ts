9f5c83a4226f094a576ed8d733e1113e8502adfd
mario8a <mario8ato@hotmail.com>
2020-12-11 23:11:13 -0600

    config de guards

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { Usuario } from '../class/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +8,14 @@ import { Socket } from 'ngx-socket-io';
 export class WebsocketService {
 
   public socketStatus = false;
+  public usuario: Usuario = null;
 
   constructor(private socket: Socket) {
+    this.cargarStorage();
     this.checkStatus();
   }
 
+  //Meotod para revisar el estado del servidor
   checkStatus() {
 
     this.socket.on('connect', () => {
@@ -37,4 +41,40 @@ export class WebsocketService {
   listen(evento: string) {
     return this.socket.fromEvent(evento);
   }
+
+  loginWS(nombre: string) {
+    // console.log('Configurando', nombre);
+
+    return new Promise((resolve, reject) => {
+
+      this.emit('configurar-usuario', {nombre}, (resp) => {
+        
+        // console.log(resp)
+        //registrar el usuario para no restablecerlo al refrescar la pagina
+        this.usuario = new Usuario(nombre);
+        this.guardarStorage();
+
+        resolve();
+      });
+
+    })
+
+  }
+
+  getUsuario() {
+    return this.usuario;
+  }
+
+  guardarStorage() {
+    localStorage.setItem('usuario', JSON.stringify(this.usuario));
+  }
+
+  cargarStorage() {
+    
+    if(localStorage.getItem('usuario')) {
+      this.usuario = JSON.parse(localStorage.getItem('usuario'));
+    }
+
+  }
+
 }
